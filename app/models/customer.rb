f43f1b73b76d742e1ef5a13f5dbb0576f2eaee05
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-03-19 21:47:47 +0000

    active admin added. added 5 customers. added customer validations.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :full_name, :phone_number, presence: true
+  validates :phone_number, length: {minimum: 7}
+  validates :image, length: {minimum: 5}
 end

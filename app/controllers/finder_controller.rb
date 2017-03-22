16bcb5ad8f695d34b9a237c7fbc7af858e895817
AndreaArdiles <h.andrea.ardiles@gmail.com>
2017-03-22 21:11:58 +0000

    part 2. added finder controller and fixed up styling

---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,15 @@
+class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def alphabetized
+    @customers = Customer.order(full_name: :asc)
+  end
+
+  def missing_email
+    #@customers = Customer.all
+    #@customers = Customer.order(email_address: :desc)
+    @customers = Customer.where(email_address: "")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'finder#index'
+
+  # below is redundant
+  #get '/', to: 'finder#index', as: 'index'
+
+  get '/customers/missing_email', to: 'finder#missing_email', as: 'missing_email'
+  get '/customers/alphabetized', to: 'finder#alphabetized', as: 'alphabetized'
+
 end
